64d1cfa7006c08a2773e7a8d16364d47c6cf82e8
Aman Rathore <aman986838@gmail.com>
2021-02-13 18:15:17 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,18 @@
+var randomNo1 =Math.floor(Math.random() * 6) + 1;
+var randomNo2 =Math.floor(Math.random() * 6) + 1;
+
+var imagesrc1="images/dice"+randomNo1+".png";
+var imagesrc2="images/dice"+randomNo2+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", imagesrc1);
+document.querySelectorAll("img")[1].setAttribute("src", imagesrc2);
+
+if(randomNo1>randomNo2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!"
+}
+else if(randomNo2>randomNo1){
+    document.querySelector("h1").innerHTML="Player 2 Wins!"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!"
+}
